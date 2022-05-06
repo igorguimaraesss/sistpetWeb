dfec88ab0f53879224c504a5919986166f16a819
Igor Murici <igor00guima@gmail.com>
2022-05-06 20:31:43 -0300

    feat: tela de admin produto

---
+++ app-front/sistpet/src/app/adm-produto/adm-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adm-produto',
+  templateUrl: './adm-produto.component.html',
+  styleUrls: ['./adm-produto.component.scss']
+})
+export class AdmProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- app-front/sistpet/src/app/app.module.ts
+++ app-front/sistpet/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AdmProdutoComponent } from './adm-produto/adm-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdmProdutoComponent
   ],
   imports: [
     BrowserModule,
