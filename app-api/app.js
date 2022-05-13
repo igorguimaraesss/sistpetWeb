6fcd31bba98adc29fadf38b1f9747eddc6b28c39
Igor Murici <igor00guima@gmail.com>
2022-05-12 23:30:33 -0300

    feat: produto endpoint

--- app-api/app.js
+++ app-api/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser"); // Necessário para interpretar o corpo das requisições HTTP
 const cors = require("cors");
-
+const produto = require("./src/api/routes/produto");
 const app = express();
 
 //Configuração dos middlewares
 app.use(cors());
+app.use(bodyParser.json()); // Converterá qualquer corpo em JSON em um objeto javascript
 
-//Configuração das rotas da API
-app.get("/", (req, res) => {
-  res.send("Express server");
-  res.end();
-});
+// //Configuração das rotas da API
+// app.get("/", (req, res) =>{
+//     res.send("Express server");
+//     res.end();
+// });
+app.use(produto);
 
 //Exporta o aplicativo express configurado
 module.exports = app;
