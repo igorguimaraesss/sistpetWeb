26d81f2f409d6ebd648b78e966bad9e079ecb7a4
Igor Murici <igor00guima@gmail.com>
2022-06-22 00:20:23 -0300

    feat: estrutura projeto frontend

---
+++ app-front/sistpet/src/app/adm-produto/adm-produto.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdmProdutoComponent } from './adm-produto.component';
+
+describe('AdmProdutoComponent', () => {
+  let component: AdmProdutoComponent;
+  let fixture: ComponentFixture<AdmProdutoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdmProdutoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdmProdutoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- app-front/sistpet/src/app/app.module.ts
+++ app-front/sistpet/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AdmProdutoComponent } from './adm-produto/adm-produto.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    AdmProdutoComponent,
+    ProdutosComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
